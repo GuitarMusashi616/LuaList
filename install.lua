ce81348b2eafba16061147400de0a53405c20f33
Austin Williams <guitarmusashi@gmail.com>
2021-12-09 14:47:44 -0900

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,33 @@
+-- for simple setup in computercraft computers by using wget run raw.githubusercontent.com/GuitarMusashi616/LuaList/main/install.lua
+
+local function clearDir(dirName)
+  shell.run("cd "..tostring(dirName))
+  shell.run("rm *.lua")
+  shell.run("cd ..")
+  shell.run("rm "..tostring(dirName))
+end
+
+local function wgetLua(repo, fileNames)
+  for i, file in pairs(fileNames) do
+    shell.run("wget "..tostring(repo)..tostring(file)..".lua")
+  end
+end
+
+local function wgetDirWithLua(dirName, repo, fileNames)
+  shell.run("mkdir "..tostring(dirName))
+  shell.run("cd "..tostring(dirName))
+  wgetLua(repo, filenames)
+  shell.run("cd ..")
+end
+
+local tArgs = {...}
+
+if tArgs[1] == "update" or tArgs[1] == "uninstall" then
+  clearDir("lib")
+end
+
+if tArgs[1] == "uninstall" then
+  error("Uninstall Successful")
+end
+
+wgetDirWithLua("lib", "raw.githubusercontent.com/GuitarMusashi616/LuaList/main/", {"list", "tbl", "util", "class"})
